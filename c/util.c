6bd038dc706b1c44554fa619ba79e282cc859c02
jglov3id <jglov3id@48e03a22-deb6-11de-b4e9-91c67fb6d0f2>
2011-04-26 07:07:30 +0000

    bingham_compose()

--- c/util.c
+++ c/util.c
@@ -713,6 +713,26 @@ void rotation_matrix_to_quaternion(double *q, double **R)
 }
 
 
+// convert a unit quaternion to a rotation matrix
+void quaternion_to_rotation_matrix(double **R, double *q)
+{
+  double a = q[0];
+  double b = q[1];
+  double c = q[2];
+  double d = q[3];
+
+  R[0][0] = a*a + b*b - c*c - d*d;
+  R[0][1] = 2*b*c - 2*a*d;
+  R[0][2] = 2*b*d + 2*a*c;
+  R[1][0] = 2*b*c + 2*a*d;
+  R[1][1] = a*a - b*b + c*c - d*d;
+  R[1][2] = 2*c*d - 2*a*b;
+  R[2][0] = 2*b*d - 2*a*c;
+  R[2][1] = 2*c*d + 2*a*b;
+  R[2][2] = a*a - b*b - c*c + d*d;
+}
+
+
 // add an element to the front of a list
 ilist_t *ilist_add(ilist_t *x, int a)
 {
