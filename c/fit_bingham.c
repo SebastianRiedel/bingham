2c5a0f04745c0374853632be4d5df226c2d67f27
jglov3id <jglov3id@48e03a22-deb6-11de-b4e9-91c67fb6d0f2>
2011-05-25 04:28:17 +0000

    added multi scatter mode

--- c/fit_bingham.c
+++ c/fit_bingham.c
@@ -78,22 +78,41 @@ int main(int argc, char *argv[])
   char *fout = (load_scatter ? argv[3] : argv[2]);
 
   int n, d;
-  bingham_t B;
+  bingham_mix_t BM;
 
   if (load_scatter) {
     double **S = load_matrix(fin, &n, &d);
-    bingham_fit_scatter(&B, S, d);
+    if (n == d) {
+      bingham_t B;
+      bingham_fit_scatter(&B, S, d);
+      double w = 1.0;
+      BM.B = &B;
+      BM.w = &w;
+      BM.n = 1;
+    }
+    else if (n > d) {  // fit multiple scatter matrices
+      BM.n = n/d;
+      safe_calloc(BM.B, BM.n, bingham_t);
+      safe_calloc(BM.w, BM.n, double);
+      int i;
+      for (i = 0; i < BM.n; i++) {
+	if (norm(S[d*i], d*d) < .00001)  // S = [0]  -->  uniform
+	  bingham_new_uniform(&BM.B[i], d);
+	else
+	  bingham_fit_scatter(&BM.B[i], &S[d*i], d);
+	BM.w[i] = 1;
+      }
+    }
   }
   else {
     double **X = load_matrix(fin, &n, &d);
+    bingham_t B;
     bingham_fit(&B, X, n, d);
+    double w = 1.0;
+    BM.B = &B;
+    BM.w = &w;
+    BM.n = 1;
   }
-
-  bingham_mix_t BM;
-  double w = 1.0;
-  BM.B = &B;
-  BM.w = &w;
-  BM.n = 1;
 
   save_bmx(&BM, 1, fout);
 
