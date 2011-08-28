7d62710f81141975d9cb131d0a4c530a900e4cff
jglov3id <jglov3id@48e03a22-deb6-11de-b4e9-91c67fb6d0f2>
2011-08-28 17:59:57 +0000

    hll caching

--- c/include/bingham/hll.h
+++ c/include/bingham/hll.h
@@ -16,6 +16,13 @@ extern "C" {
 
 
   typedef struct {
+    kdtree_t *Q_kdtree;
+    double **X;
+    double ***S;
+    int n;
+  } hll_cache_t;
+
+  typedef struct {
     double **Q;    /* input (S^{dq-1}) */
     double **X;    /* output (R^dx) */
     int n;         /* number of samples (rows) in X,Q */
@@ -25,11 +32,14 @@ extern "C" {
     double *x0;    /* prior mean */
     double **S0;   /* prior covariance */
     double w0;     /* prior weight */
+
+    hll_cache_t cache;
   } hll_t;
 
 
   void hll_new(hll_t *hll, double **Q, double **X, int n, int dq, int dx);
   void hll_free(hll_t *hll);
+  void hll_cache(hll_t *hll, double **Q, int n);
   void hll_sample(double **X, double ***S, double **Q, hll_t *hll, int n);
 
 
