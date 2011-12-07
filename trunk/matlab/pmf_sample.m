b193813ca11aec188d62308a6003c7e64066722b
jglov3id <jglov3id@48e03a22-deb6-11de-b4e9-91c67fb6d0f2>
2011-12-07 13:33:21 +0000

    computed new bingham constants

--- trunk/matlab/pmf_sample.m
+++ trunk/matlab/pmf_sample.m
@@ -2,6 +2,12 @@ function i = pmf_sample(W)
 %i = pmf_sample(W) -- draw a sample from a discrete probability
 %distribution
 
+if size(W,1)>1
+    W = W';
+end
+
+W = W/sum(W);  % normalize
+
 r = rand();
 C = cumsum(W);
 i = find(([0 C(1:end-1)]<r).*(C>=r));
