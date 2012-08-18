e6aa51fe873ce2508f2f2c6830f95fb091d9ff53
jglov3id <jglov3id@gmail.com>
2012-08-18 16:19:08 +0000

    range image bug fixes, unnormalized bingham_mult, speed optimizations

--- matlab/tools/interp_linear.m
+++ matlab/tools/interp_linear.m
@@ -8,9 +8,15 @@ if s(end)==1
 end
 d = length(s);
 
-Z = reshape(X, [1,2^d]);
-for i=d:-1:1
-    k = length(Z)/2;
-    Z = Z(1:k) + alpha(i)*(Z(k+1:end) - Z(1:k));
+Z = X(1:end); %reshape(X, [1,2^d]);
+if d==3  % optimization for typical case
+    Z = Z(1:4) + alpha(3)*(Z(5:8) - Z(1:4));
+    Z = Z(1:2) + alpha(2)*(Z(3:4) - Z(1:2));
+    y = Z(1) + alpha(1)*(Z(2) - Z(1));
+else
+    for i=d:-1:1
+        k = 2^(i-1); %length(Z)/2;
+        Z = Z(1:k) + alpha(i)*(Z(k+1:end) - Z(1:k));
+    end
+    y = Z;
 end
-y = Z;
