2214522fdc979e3a3d10607993af2d3d7e4590d6
madratman <ratneshmadaan@gmail.com>
2015-07-02 03:18:55 -0400

    fixed deprecated bingham_cluster.m

--- matlab/deprecated/bingham_cluster.m
+++ matlab/deprecated/bingham_cluster.m
@@ -5,14 +5,24 @@ if nargin < 2
    min_points = 20;
 end
 
-for i=1:100
+for i=1:6
 
    [B(i) outliers] = bingham_fit_mlesac(X);
-   cnts(i) = size(X,2) - length(outliers);
+%    outliers
+%   ouliers is a row vector
+%    size(X,2)
+%    i
+   cnts(i) = size(X,1) - length(outliers);
    
    if length(outliers) < min_points
       break;
    end
-   
-   X = X(:, outliers);
+  X_updater = zeros(length(outliers), size(X,2));
+%    size(X,1)
+%    size(X,2)
+  for j = 1:length(outliers)
+    X_updater(j,:) = X(outliers(j),:);
+  end
+  X = X_updater;
+  size(X)
 end
