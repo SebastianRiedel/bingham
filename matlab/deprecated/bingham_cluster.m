29938ddaa10c357eb45c94e3ffdc23d21761c674
madratman <ratneshmadaan@gmail.com>
2015-07-02 04:15:44 -0400

    adds comments, fixes upper bound back to 100

--- matlab/deprecated/bingham_cluster.m
+++ matlab/deprecated/bingham_cluster.m
@@ -1,28 +1,24 @@
-function [B cnts] = bingham_cluster(X, min_points)
-% [B cnts] = bingham_cluster(X, min_points) -- where each B(i) contains fields V, Z, F
+function [B weights] = bingham_cluster(X, min_points)
+% [B weights] = bingham_cluster(X, min_points) -- where each B(i) contains fields V, Z, F
 
 if nargin < 2
    min_points = 20;
 end
 
-for i=1:6
-
-   [B(i) outliers] = bingham_fit_mlesac(X);
-%    outliers
-%   ouliers is a row vector
-%    size(X,2)
-%    i
-   cnts(i) = size(X,1) - length(outliers);
-   
-   if length(outliers) < min_points
-      break;
-   end
+for i=1:100 %max no of clusters
+  [B(i) outliers] = bingham_fit_mlesac(X);
+  %   ouliers is a row vector
+  weights(i) = size(X,1) - length(outliers);
+  % weight of a particular bingham is no of points left - no of ouliers
+  if length(outliers) < min_points
+    break;
+  end
   X_updater = zeros(length(outliers), size(X,2));
-%    size(X,1)
-%    size(X,2)
+  % initialize the outlier data for next iteration to zeros
   for j = 1:length(outliers)
     X_updater(j,:) = X(outliers(j),:);
   end
+  % get them outliers
   X = X_updater;
-  size(X)
+  % update the outliers for the next iteration
 end
